4bd98e1820b403b249a6b7c48bf91d3c9536e5e6
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2022-05-16 20:36:17 +0300

    4.2

---
+++ JS/4.2/script.js
@@ -0,0 +1,12 @@
+function lengthConverter(val) {
+  document.getElementById("outputInch").innerHTML = val * 0.3937;
+}
+
+alert(function ([]) {
+  c = 0;
+  for (i in []) {
+    if (i % 2 == 0) {
+      return c++;
+    } else console.log(error);
+  }
+})[(10, 4, 3, 4, 5)];
